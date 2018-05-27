e02f20fa52424484755e832256b7a702f3fcb7ee
jdimasw <dwdimas@gmail.com>
2018-05-27 19:08:11 +0700

    new

--- iot_mean_single.c
+++ iot_mean_single.c
@@ -4,33 +4,32 @@
 
 #define bufSize 1024
 
-int main(int argc, char *argv[])
-{
-  FILE* fp;
-  char buf[bufSize];
-  if (argc != 2)
-  {
-    fprintf(stderr,
-            "Usage: %s <soure-file>\n", argv[0]);
-    return 1;
-  }
-  if ((fp = fopen(argv[1], "r")) == NULL)
-  { /* Open source file. */
-    perror("fopen source-file");
-    return 1;
-  }
+int main(int argc, char *argv[]){	
+	FILE* fp;
+	char buf[bufSize];
+	if (argc != 2){
+		fprintf(stderr,
+		"Usage: %s <soure-file>\n", argv[0]);
+		return 1;
+	}
+	if ((fp = fopen(argv[1], "r")) == NULL){ 
+	//ini apa sih
+		/* Open source file. */
+		perror("fopen source-file");
+		return 1;
+	}
 
-  int count = 0;
+	int count = 0;
 
-  while (fgets(buf, sizeof(buf), fp) != NULL)
-  {
-    buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
-    //printf("%s\n", buf);
-    count = count + 1;
-  }
+	while(fgets(buf, sizeof(buf), fp) != NULL){
+		buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
+		//printf("%s\n", buf);
+		count = count + 1;
+	}
+	
 
-  printf("%d \n", count);
+	printf("%d \n %s \n", count, &buf);
 
-  fclose(fp);
-  return 0;
+	fclose(fp);
+	return 0;
 }
