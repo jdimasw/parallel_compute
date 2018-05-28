ff6b20ecac30abe758f314fd3d313d32056fe403
jdimasw <39666377+jdimasw@users.noreply.github.com>
2018-05-28 14:45:12 +0700

    Update test.c

--- test.c
+++ test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <json/json.h>
 
 #define bufSize 1024
 
@@ -20,15 +21,22 @@ int main(int argc, char *argv[])
   }
 
   int count = 0;
+  int val = 0;
 
   while (fgets(buf, sizeof(buf), fp) != NULL)
   {
     buf[strlen(buf) - 1] = '\0'; // eat the newline fgets() stores
     //printf("%s\n", buf);
     count = count + 1;
+    
+    json_object *jobj = json_tokener_parse(buf);
+    json_object *obj_hum = json_object_object_get(jobj,"humidity");
+
+    const int *hum_val = int(json_object_get_string(obj_hum));
+    val = hum_val;
   }
 
-  printf("%d \n", count);
+  printf("%d \n%d\n", count, val);
 
   fclose(fp);
   return 0;
